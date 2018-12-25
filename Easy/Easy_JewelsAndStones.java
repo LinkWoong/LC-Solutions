4ff8100a13b72557573f4e6d58a58b034b3563f5
LinkWoong <Link.wong095@gmail.com>
2018-12-25 22:02:50 +0800

    Add Easy_JewelsAndStones

--- Easy/Easy_JewelsAndStones.java
+++ Easy/Easy_JewelsAndStones.java
@@ -1,4 +1,7 @@
 package Leetcode;
+
+import java.util.HashSet;
+
 /*
     You're given strings J representing the types of stones that are jewels,
     and S representing the stones you have.  Each character in S is a type of stone you have.
@@ -23,11 +26,61 @@ package Leetcode;
 */
 public class Easy_JewelsAndStones {
     public static void main(String[] args){
-        System.out.println("ass");
+        String J = "wch";
+        String S = "abcAssDDCCChcHH";
+        int result = 0;
+
+        result = numJewelsInStones_v1(J, S);
+        System.out.println(result);
+
+        result = numJewelsInStones_v2(J, S);
+        System.out.println(result);
+
+        result = numJewelsInStones_v3(J, S);
+        System.out.println(result);
     }
 
-    public int numJewelsInStones(String J, String S){
-        return 0;
+    // Runtime: 14 ms, faster than 32.60%
+    private static int numJewelsInStones_v1(String J, String S){
+        int count = 0;
+
+        for(char J_sub: J.toCharArray()){
+            for(char S_sub: S.toCharArray()){
+                if(J_sub == S_sub){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    //Runtime: 10 ms, faster than 65.10% of Java online submissions
+    // 利用了HashSet自动计算HashCode来考虑唯一性，很自然的想法
+    private static int numJewelsInStones_v2(String J, String S){
+        HashSet<Character> hs = new HashSet<>();
+        for(char J_sub: J.toCharArray()){
+            hs.add(J_sub);
+        }
+
+        int count = 0;
+        for(char S_sub: S.toCharArray()){
+            if(hs.contains(S_sub)){
+                count++;
+            }
+        }
+
+        return count;
+    }
+    
+    private static int numJewelsInStones_v3(String J, String S){
+        int count = 0;
+        for(char S_sub: S.toCharArray()){
+            if(J.indexOf(S_sub) != -1){
+                count++;
+            }
+        }
+        return count;
     }
 
 }

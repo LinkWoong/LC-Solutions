bc0ac0ad7fc208a2de3f34293e028e603534bc79
Mingyang Li <lmy961108@gmail.com>
2018-12-28 00:01:47 +0800

    Add 121

---
+++ DynamicProgramming/Easy_BestTimeToBuyAndSellStock_121_LMY.java
@@ -0,0 +1,40 @@
+package LeetCode;
+
+public class Easy_BestTimeToBuyAndSellStock_121_LMY {
+
+        /*
+        Say you have an array for which the ith element is the price of a given stock on day i.
+
+    If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+    Note that you cannot sell a stock before you buy one.
+
+    Example 1:
+
+    Input: [7,1,5,3,6,4]
+    Output: 5
+    Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+                 Not 7-1 = 6, as selling price needs to be larger than buying price.
+    Example 2:
+
+    Input: [7,6,4,3,1]
+    Output: 0
+    Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+     */
+
+    //d[i]代表[0,i]内交易股票的最大收益 minPrice表示［0，i）之间价格的最小值
+    //状态转移方程 dp[i] = max(dp[i-1],prices[i]-minPrice)
+    public int maxProfit(int[] prices){
+        if(prices == null || prices.length == 0)
+            return 0;
+        int[]dp = new int[prices.length];
+        int minPrice = prices[0];
+        for(int i = 1; i< prices.length; i++){
+            minPrice = Math.min(minPrice,prices[i]);
+            dp[i] = Math.max(dp[i-1],prices[i]-minPrice);
+        }
+        return dp[prices.length - 1];
+
+    }
+}

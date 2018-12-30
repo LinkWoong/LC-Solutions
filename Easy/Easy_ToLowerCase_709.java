908909bf513d11a749b50c7ddfda9c799db2fb56
LinkWoong <Link.wong095@gmail.com>
2018-12-30 17:40:07 +0800

    Add 709

---
+++ Easy/Easy_ToLowerCase_709.java
@@ -0,0 +1,54 @@
+package Leetcode;
+/*
+    Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+    Example 1:
+
+    Input: "Hello"
+    Output: "hello"
+    Example 2:
+
+    Input: "here"
+    Output: "here"
+    Example 3:
+
+    Input: "LOVELY"
+    Output: "lovely"
+ */
+
+public class Easy_ToLowerCase_709 {
+    public static void main(String[] args){
+        String s = "Hello";
+
+        // String result = toLowerCase(s);
+        // System.out.println(result);
+
+        String result = toLowerCase_v2(s);
+        System.out.println(result);
+    }
+
+    // 利用自带的的toLowerCase()
+    private static String toLowerCase(String str){
+        return str.toLowerCase();
+    }
+
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions
+    private static String toLowerCase_v2(String str){
+        char[] result = new char[str.length()];
+
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            if(c >= 65 && c <= 90){
+                result[i] = (char) (c + 32);
+            }
+            else{
+                result[i] = c;
+            }
+        }
+        return new String(result);
+    }
+
+    private static String toLowerCase_v3(String str){
+        return null;
+    }
+}

fd721f24c6e85ea7ff91dbaca814d45fc37790a5
LinkWoong <Link.wong095@gmail.com>
2019-04-12 23:55:46 +0800

    Add 203

--- LinkedList/Easy_RMLinkedList_203.java
+++ LinkedList/Easy_RMLinkedList_203.java
@@ -10,13 +10,11 @@ package Leetcode;
 */
 
 public class Easy_RMLinkedList_203 {
-
-    public static void main(String[] args){
-
-    }
+    // 思路：由于是剔除所有符合要求的element，就要重新构造一条链表
+    // 弄一个dummyHead，并让其next指向head，再用current遍历剔除元素
+    // 如果current.val == val，就跳过这个元素，跳过的实现是让prev（另一条链表）next指向current.next
     public ListNode removeElements(ListNode head, int val) {
-        // 删除所有开始的符合条件的node
-        ListNode dummyHead = new ListNode(-1);
+        ListNode dummyHead = new ListNode(0);
         dummyHead.next = head;
 
         //删除中间&末尾符合条件的node
@@ -38,5 +36,24 @@ public class Easy_RMLinkedList_203 {
 
         head.next = removeElements_v2(head.next, val);
         return head.val == val ? head.next : head;
+    }
+
+    public ListNode removeElements_v3(ListNode head, int val) {
+        if(head == null){
+            return null;
+        }
+        ListNode newHead = new ListNode(0);
+        newHead.next = head;
+
+        ListNode current = head, prev = newHead;
+        while(current != null){
+            if(current.val == val){
+                prev.next = current.next;
+            } else{
+                prev = prev.next;
+            }
+            current = current.next;
+        }
+        return newHead.next;
     }
 }

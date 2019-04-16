4a4e56831885bdca26819e2d84c37a3a220bf9e7
LinkWoong <Link.wong095@gmail.com>
2019-04-16 21:53:43 +0800

    Add 89

--- BinaryTree/Medium_ValidateBTree_98.java
+++ BinaryTree/Medium_ValidateBTree_98.java
@@ -45,8 +45,24 @@ public class Medium_ValidateBTree_98 {
             prev = root;
             root = root.right;
         }
-
         return true;
+    }
 
+    public boolean isValidBST_recursive(TreeNode root){
+        if(root == null)
+            return true;
+        return helper(root, null, null);
+    }
+
+    private boolean helper(TreeNode node, TreeNode min, TreeNode max){
+        if(node == null)
+            return true;
+        if(min != null && node.val <= min.val){
+            return false;
+        }
+        if(max != null && node.val >= max.val){
+            return false;
+        }
+        return helper(node.left, min, node) && helper(node.right, node, max);
     }
 }

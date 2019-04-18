50b832103dd5031a6b68540cf66fc1665357ef56
LinkWoong <Link.wong095@gmail.com>
2019-04-18 14:43:21 +0800

    Add 414

--- Array/Easy_thirdMax_414.java
+++ Array/Easy_thirdMax_414.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Easy_thirdMax_414 {
     // 思路：利用优先队列直接出结果，要注意队列size == 0的情况
-    public int thirdMax(int[] nums) {
+    public static int thirdMax(int[] nums) {
         // Comparator<Integer> comparator = new helper();
         // PriorityQueue<Integer> queue = new PriorityQueue<>(comparator);
         PriorityQueue<Integer> queue = new PriorityQueue<>();
@@ -18,7 +18,7 @@ public class Easy_thirdMax_414 {
                 }
             }
         }
-
+        System.out.println(queue);
         if(queue.size() < 3){
             while(queue.size() > 1)
                 queue.poll();
@@ -26,10 +26,25 @@ public class Easy_thirdMax_414 {
         return queue.size() == 0 ? -1 : queue.peek();
     }
 
-    static class helper implements Comparator<Integer>{
-        @Override
-        public int compare(Integer o1, Integer o2) {
-            return o1.compareTo(o2);
+    public static int thirdMax_v2(int[] nums){
+        Integer max1 = null, max2 = null, max3 = null;
+        for(Integer i: nums){
+            if(i.equals(max1) || i.equals(max2) || i.equals(max3)){
+                continue;
+            }
+            else{
+                if(max1 == null || i > max1){
+                    max3 = max2;
+                    max2 = max1;
+                    max1 = i;
+                } else if(max2 == null || i > max2){
+                    max3 = max2;
+                    max2 = i;
+                } else if(max3 == null || i > max3){
+                    max3 = i;
+                }
+            }
         }
+        return max3 == null ? max1 : max3;
     }
 }

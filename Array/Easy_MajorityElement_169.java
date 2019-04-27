8d1436ae5dd3e60d93d928f0a1e11be346ba3baf
LinkWoong <Link.wong095@gmail.com>
2019-04-27 16:01:52 +0800

    Add 169

--- Array/Easy_MajorityElement_169.java
+++ Array/Easy_MajorityElement_169.java
@@ -18,20 +18,40 @@ import java.util.Map;
     Output: 2
  */
 public class Easy_MajorityElement_169 {
+    // 思路：利用了Boyer-Moore majority vote算法
+    // 从index为1的元素开始sweep，如果nums[i]和major一样，count++
+    // 否则count--；当count为0时把major替换为当前元素nums[i]
+    public int majorityElement(int[] nums){
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int major = nums[0], count = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(count == 0){
+                count++;
+                major = nums[i];
+            } else if(major == nums[i]){
+                count++;
+            } else{
+                count--;
+            }
+        }
+        return major;
+    }
     // 思路：计算每个元素出现次数，返回符合条件的即可
-    public int majorityElement(int[] nums) {
+    public int majorityElement_v2(int[] nums) {
         int n = nums.length / 2;
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i: nums){
-            if(!map.containsKey(i))
+        for(int i: nums) {
+            if (!map.containsKey(i))
                 map.put(i, 1);
             else
                 map.put(i, map.getOrDefault(i, 0) + 1);
+
+            if (map.get(i) > n)
+                return i;
         }
-        for(Integer key: map.keySet()){
-            if(map.get(key) > n)
-                return key;
-        }
+
         return -1;
     }
 }
